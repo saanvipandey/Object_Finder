f01de14435a1c5d5098c8819bf7617230c5b57ef
saanvipandey <74305880+saanvipandey@users.noreply.github.com>
2021-10-03 11:38:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
+status = "";
+
 function setup()
 {
     canvas = createCanvas(600, 250);
     canvas.position(390, 320);
+    video = createCapture(VIDEO);
+    video.size(600, 250);
+    video.hide();
+}
+
+function draw()
+{
+    image(video, 0, 0, 600, 500);
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status = true;
+    
 }
